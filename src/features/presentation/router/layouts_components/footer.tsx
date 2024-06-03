6c06e52c78f591c693e5c619dd12c02ff0b59951
Juana Estarli <jestarli@asap-consulting.net>
2024-06-03 16:19:02 -0300

    cambios footer

--- src/features/presentation/router/layouts_components/footer.tsx
+++ src/features/presentation/router/layouts_components/footer.tsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faLinkedin, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 const InnerContainer = styled.div`
     width: 65%;
@@ -8,14 +10,14 @@ const InnerContainer = styled.div`
 
 const LogoContainer = styled.div`
 img {
-    width: 150px;
-    margin-bottom: 20px;
+    width: 9.375rem;
+    margin-bottom: 1.25rem;
 }
 `
 
 const FooterInner = styled.div`
     display: flex;
-    margin-top: 50px;
+    margin-top: 3.125rem;
 `
 
 const DatosContainer = styled.div`
@@ -23,21 +25,39 @@ const DatosContainer = styled.div`
     flex-direction: column;
 `
 
+const SocialIcons = styled.div`
+  display: flex;
+  justify-content: left;
+  gap: 0.625rem;
+  margin-top: 0.625rem;
+
+  a {
+    color: white;
+    font-size: 15px;
+    width: 2rem;
+    height: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    }
+`;
+
 const EmpresaContainer = styled.ul`
     list-style-type: none;
-    padding-left: 60px;
+    padding-left: 3.75rem;
     margin: 0;
 `
 
 const SolucionesContainer = styled.ul`
     list-style-type: none;
-    padding-left: 60px;
+    padding-left: 3.75rem;
     margin: 0;       
 `
 
 const ExtraContainer = styled.ul`
     list-style-type: none;
-    padding-left: 60px;
+    padding-left: 3.75rem;
     margin: 0;
 `
 
@@ -46,7 +66,7 @@ const DatosSpan = styled.span`
     font-family: Calibri;
     color: #7A7A7A;
     font-size: 20px;
-    margin-bottom: 15px;
+    margin-bottom: 0.938rem;
 `
 
 const Item = styled.li`
@@ -54,7 +74,7 @@ const Item = styled.li`
     color: #7A7A7A;
     font-size: 20px;
     font-family: Calibri;
-    line-height: 35px;
+    line-height: 2.188rem;
 `
 
 const ItemH1 = styled.li`
@@ -62,8 +82,8 @@ const ItemH1 = styled.li`
     font-family: Calibri;
     color: #565b62;
     font-weight: 600;
-    line-height: 45px;
-    margin-top: 8px;
+    line-height: 2.813rem;
+    margin-top: 0.5rem;
 `
 
 const FooterContainer = styled.div`
@@ -76,12 +96,22 @@ const FooterContainer = styled.div`
     background-size: cover;
     background-position-y: 28%;
     position: absolute;
-    margin-top: 45px;
+    margin-top: 2.813rem;
 `
 const HR = styled.hr`
     background-color: #68bc9352;
     border: none;
-    height: 2px;
+    height: 0.125rem;
+`
+const InstaIcon= styled.a`
+    background-color: #E82096ED;;
+`
+const WppIcon = styled.a`
+    background-color: #25d366;
+`
+const LinIcon = styled.a`
+    background-color: #0077b5;
+
 `
 
 const Footer = () => {
@@ -98,15 +128,28 @@ const Footer = () => {
                             <br/>HIT Cel, Nuñez
                         </DatosSpan>
                         <DatosSpan>ASAP CONSULTING © 2022</DatosSpan>
+                        <DatosSpan>
+                            <SocialIcons>
+                                <WppIcon href="https://api.whatsapp.com/send?phone=5491135792713" target="_blank" rel="noopener noreferrer">
+                                    <FontAwesomeIcon icon={faWhatsapp} />
+                                </WppIcon>
+                                <LinIcon href="https://www.linkedin.com/company/asap-consulting-s.a./" target="_blank" rel="noopener noreferrer">
+                                    <FontAwesomeIcon icon={faLinkedin} />
+                                </LinIcon>
+                                <InstaIcon href="https://www.instagram.com/accounts/login/?next=%2Fasap.consulting%2F" target="_blank" rel="noopener noreferrer">
+                                    <FontAwesomeIcon icon={faInstagram} />
+                                </InstaIcon>
+                            </SocialIcons>
+                        </DatosSpan>
                     </DatosContainer>
                     <EmpresaContainer>
                         <ItemH1>Empresa</ItemH1>
                         <Item>Nosotros</Item>
                         <Item>Nuestro Blog</Item>
-                        <Item>Capacitacion</Item>
+                        <Item>Capacitación</Item>
                         <Item>Clientes</Item>
-                        <Item>Casos de exito</Item>
-                        <Item>Politica de privacidad</Item>
+                        <Item>Casos de éxito</Item>
+                        <Item>Política de privacidad</Item>
                     </EmpresaContainer>
                     <SolucionesContainer>
                         <ItemH1>Soluciones de capital humano</ItemH1>
@@ -116,9 +159,9 @@ const Footer = () => {
                         <Item>Saas Wisoft</Item>
                     </SolucionesContainer>
                     <ExtraContainer>
-                        <ItemH1>Capacitacion</ItemH1>
+                        <ItemH1>Capacitación</ItemH1>
                         <ItemH1>Beneficios</ItemH1>
-                        <ItemH1>Busquedas Laborales</ItemH1>
+                        <ItemH1>Búsquedas Laborales</ItemH1>
                     </ExtraContainer>
                 </FooterInner>
             </InnerContainer>
