880318deef1a9f79ee861ecf39dd8d63b172cb89
Juana Estarli <jestarli@asap-consulting.net>
2024-07-11 16:55:45 -0300

    text jr squad

--- src/features/presentation/screens/soluciones-servicios/JR-squad/JR-squad.styles.tsx
+++ src/features/presentation/screens/soluciones-servicios/JR-squad/JR-squad.styles.tsx
@@ -2,5 +2,26 @@ import styled from 'styled-components';
 
 export const JrContainer = styled.div`
     width: 100vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     padding-top: 13vh;
+    gap: 50px;
+`;
+
+export const BannerContainer = styled.div`
+    width: 100%;
+`;
+
+export const TextContainer = styled.div`
+`;
+
+export const Text = styled.p`
+    font-size: 22px;
+    text-align: center;
+    color: #7a7a7a;
+    line-height: 1.5em;
+    font-weight: 400;
+    margin: 0;
+    max-width: 1200px; 
 `;
--- src/features/presentation/screens/soluciones-servicios/JR-squad/JR-squad.tsx
+++ src/features/presentation/screens/soluciones-servicios/JR-squad/JR-squad.tsx
@@ -1,9 +1,16 @@
-import { JrContainer } from './JR-squad.styles';
+import { JrContainer, TextContainer, Text, BannerContainer } from './JR-squad.styles';
 import Banner from '../../../components/ui_components/banner/banner';
 const JRsquadScreen = () => {
     return (
         <JrContainer>
-            <Banner title={"JR Squad"} extra={<>Nuestro lema es <strong>“no hay senior, sin junior”</strong>, es por eso que nos enfocamos en la generación de nuevos talentos.</>}></Banner>
+            <BannerContainer>
+                <Banner title={"JR Squad"} extra={<>Nuestro lema es <strong>“no hay senior, sin junior”</strong>, es por eso que nos enfocamos en la generación de nuevos talentos.</>}></Banner>
+            </BannerContainer>
+            <TextContainer>
+                <Text>
+                Los Juniors Squads ofrecen la posibilidad a las empresas de incorporar nuevos talentos sin tener que afrontar su curva de aprendizaje. Se trata de equipos con <strong>perfiles juniors, especializados en la tecnología que requiera el cliente.</strong>
+                </Text>
+            </TextContainer>
         </JrContainer>
     );
 };
