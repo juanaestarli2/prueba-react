385caa05fda2d29fd781df2e2fc7a2b72e8af39d
giulianagimenez-admin <ggimenez@asap-consulting.net>
2024-07-16 12:48:57 -0300

    cards

--- src/features/presentation/screens/soluciones-servicios/managed-services/managed-services.tsx
+++ src/features/presentation/screens/soluciones-servicios/managed-services/managed-services.tsx
@@ -1,4 +1,4 @@
-import { ManagedContainer, Container, Image, ContainerBox, ContainerSec, ContainerBoxSec } from './managed.style';
+import { ManagedContainer, Container, Image, ContainerBox, ContainerSec, ContainerBoxSec, ContainerSecundario } from './managed.style';
 import Banner from '../../../components/ui_components/banner/banner';
 import Card from '../../../components/ui_components/card/card';
 import CardContacto from '../../../components/ui_components/cardContacto/cardContacto';
@@ -12,22 +12,22 @@ const ManagedServicesScreen = () => {
                 <Container>
                     <ContainerBox>
                         <Image src="https://asap-consulting.net/wp-content/uploads/2023/06/ms1.png" alt="Image 1" />
-                        <Card text={<>Las empresas de hoy requieren el mejor uso de <strong>las nuevas tecnologías que optimizan su negocio y permite adecuarse rápidamente a mercados</strong> en constante cambio.</>}></Card>
+                        <Card text={<>Las empresas de hoy requieren el mejor uso de <strong>las nuevas tecnologías que optimizan su negocio y permite adecuarse rápidamente a mercados</strong> en constante cambio.</>} height="5rem" justifyContent= 'center'></Card>
                     </ContainerBox>
                     <ContainerBox>
-                        <Card text={<>Nuestro equipo de <strong>profesionales altamente calificados y capacitados</strong>, combinado con el uso de los últimos avances en tecnología y procesos, nos permite brindar resultados más efectivos y eficientes.</>}></Card>
-                        <Image src="https://asap-consulting.net/wp-content/uploads/2023/06/m2s.png" alt="Image 2" />
+                        <Card text={<>Nuestro equipo de <strong>profesionales altamente calificados y capacitados</strong>, combinado con el uso de los últimos avances en tecnología y procesos, nos permite brindar resultados más efectivos y eficientes.</>} height="5rem"  justifyContent= 'center'></Card>
+                        <Image src="https://asap-consulting.net/wp-content/uploads/2023/06/m2s.png" alt="Image 2"/>
                     </ContainerBox>
                 </Container>
-                <div>
-                <ContainerSec>
-                    {ManagedServicesCardData.map((card: { text: ReactElement<string>; imageSrc: string }, index: Key) => (
-                    <ContainerBoxSec key={index}>
-                        <Card text={card.text} imageSrc={card.imageSrc}/>
-                    </ContainerBoxSec>
-                    ))}
-                </ContainerSec>
-                </div>
+                <ContainerSecundario>
+                    <ContainerSec>
+                        {ManagedServicesCardData.map((card: { text: ReactElement<string>; imageSrc: string }, index: Key) => (
+                        <ContainerBoxSec key={index}>
+                            <Card text={card.text} imageSrc={card.imageSrc}/>
+                        </ContainerBoxSec>
+                        ))}
+                    </ContainerSec>
+                </ContainerSecundario>
             <CardContacto></CardContacto>
         </ManagedContainer>
     );
